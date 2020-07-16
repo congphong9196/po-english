3793c9a8ebd96a3601d920005ca365c039509aee
Nguyen Cong. Phong <congphong9196@gmail.com>
2020-07-16 19:53:09 +0700

    change some things

--- app/src/main/java/com/example/recyclerview/fragment/LearningFragment.java
+++ app/src/main/java/com/example/recyclerview/fragment/LearningFragment.java
@@ -1,12 +1,20 @@
 package com.example.recyclerview.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.recyclerview.R;
+import com.example.recyclerview.activity.WordListActivity;
+import com.example.recyclerview.data.DatabaseHelper;
+import com.example.recyclerview.data.Word;
+import com.example.recyclerview.data.WordDAO;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +22,10 @@ import androidx.fragment.app.Fragment;
 
 public class LearningFragment extends Fragment {
     private final OnLearningFragmentNextListener listener;
+    private TextView tvTopicWordName;
+    private ImageView imgImage;
+    private TextView tvMean;
+
 
     public LearningFragment(OnLearningFragmentNextListener listener) {
         this.listener = listener;
@@ -23,6 +35,10 @@ public class LearningFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_learn, container, false);
+
+        tvTopicWordName = v.findViewById(R.id.tv_topic_word_name);
+        imgImage = v.findViewById(R.id.img_image);
+        tvMean = v.findViewById(R.id.tv_mean);
         Button button = v.findViewById(R.id.btnNext);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +46,11 @@ public class LearningFragment extends Fragment {
                 listener.OnLearningFragmentNext();
             }
         });
+
+//        getWordInIntent();
         return v;
+
+
     }
 
 //    @Override
@@ -41,6 +61,20 @@ public class LearningFragment extends Fragment {
     public interface OnLearningFragmentNextListener {
         void OnLearningFragmentNext();
     }
+//    public void getWordInIntent() {
+//        Intent intent = getIntent();
+//        int wordId = intent.getIntExtra(WordListActivity.WORD_ID, -1);
+//        if (wordId == -1) {
+//            Toast.makeText(getContext(), "Wrong word id", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//        DatabaseHelper helper = new DatabaseHelper(getContext());
+//        WordDAO wordDAO = new WordDAO(helper);
+//        Word word = wordDAO.getWordById(wordId);
+//        tvMean.setText(word.getMeaning());
+//        tvTopicWordName.setText(word.getValue());
+//      //  imgImage.setImageResource(word.get());
+//    }
 
 
 }
